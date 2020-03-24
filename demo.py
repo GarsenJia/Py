5b79de9cbbb72a2c55380e5ffe0f8d5c3d023fc7
GarsenJia <55094877+GarsenJia@users.noreply.github.com>
2020-03-24 16:07:54 -0400

    updated python file and input file

--- demo.py
+++ demo.py
@@ -2,26 +2,75 @@
 import wx
 import noname
 import random
+import pyperclip as clip
 
-list = []
+save = []
 favorite = []
-#f = open("input.txt")
-f = open("input.txt", encoding='utf-8')
+f = open("./input.txt", encoding='utf-8')
+line = f.readline()
 line = f.readline()
 while line:
-    list.append(line)
+    save.append(line)
+    line = f.readline()
+    if line == "favorite:\n":
+        line = f.readline()
+        break
+while line:
+    favorite.append(line)
     line = f.readline()
 f.close()
+
 
 class TFrame(noname.MyFrame1):
 
     def __init__(self, parent):
         noname.MyFrame1.__init__(self, parent)
-        trivial = list
+
+    def saveAll(self, event):
+        f = open("input.txt", "w", encoding='utf-8')
+        f.seek(0)
+        f.truncate()
+        f.writelines("save:\n")
+        for s in save:
+            f.writelines(s)
+        f.writelines("favorite:\n")
+        for s in favorite:
+            f.writelines(s)
+        f.close()
+        wx.Exit()
+
 
     def generate(self, event):
-        s = list[random.randint(0, len(list))]
+        s = save[random.randint(0, len(save) - 1)]
         self.m_textCtrl1.SetValue(s)
+
+    def copyto(self, event):
+        s = self.m_textCtrl1.Value
+        clip.copy(s)
+
+    def add(self, event):
+        s = self.m_textCtrl1.Value
+        if(s in save and not(s in favorite)):
+            favorite.append(self.m_textCtrl1.Value)
+
+    def generatefavorite(self, event):
+        try:
+            s = favorite[random.randint(0, len(favorite) - 1)]
+            self.m_textCtrl1.SetValue(s)
+        except:
+            self.m_textCtrl1.SetValue("MyFavorite is empty!")
+        finally:
+            pass
+
+    def remove(self, event):
+        try:
+            s = self.m_textCtrl1.Value
+            favorite.remove(s)
+        except:
+            self.m_textCtrl1.SetValue("Not in MyFavorite!")
+
+    def clear(self, event):
+        favorite.clear()
 
 
 def main():
@@ -29,6 +78,7 @@ def main():
     frame = TFrame(None)
     frame.Show(True)
     app.MainLoop()
+
 
 if __name__ == '__main__':
     main()
--- noname.py
+++ noname.py
@@ -3,35 +3,77 @@ import wx
 import wx.xrc
 
 
-class MyFrame1 ( wx.Frame ):
-	
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"This is trivial!", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-		
-		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
-		
-		bSizer1 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.Point( -1,-1 ), wx.Size( 400,-1 ), 0 )
-		bSizer1.Add( self.m_textCtrl1, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-		
-		self.m_button1 = wx.Button( self, wx.ID_ANY, u"I'm feeling lucky", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer1.Add( self.m_button1, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-		
-		
-		self.SetSizer( bSizer1 )
-		self.Layout()
-		
-		self.Centre( wx.BOTH )
-		
-		# Connect Events
-		self.m_button1.Bind( wx.EVT_BUTTON, self.generate )
-	
-	def __del__( self ):
-		pass
-	
-	
-	def generate( self, event ):
-		event.Skip()
-	
+class MyFrame1(wx.Frame):
 
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"This is trivial!", pos=wx.DefaultPosition,
+                          size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+
+        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_textCtrl1 = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.Point(-1, -1), wx.Size(400, -1), 0)
+        bSizer1.Add(self.m_textCtrl1, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        gSizer1 = wx.GridSizer(0, 2, 0, 0)
+
+        self.m_button1 = wx.Button(self, wx.ID_ANY, u"I'm feeling lucky", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer1.Add(self.m_button1, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_button2 = wx.Button(self, wx.ID_ANY, u"copy to clipboard", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer1.Add(self.m_button2, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_button3 = wx.Button(self, wx.ID_ANY, u"add to MyFavorite", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer1.Add(self.m_button3, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_button4 = wx.Button(self, wx.ID_ANY, u"choose one from MyFavorite", wx.DefaultPosition, wx.DefaultSize,
+                                   0)
+        gSizer1.Add(self.m_button4, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_button5 = wx.Button(self, wx.ID_ANY, u"remove from MyFavorite", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer1.Add(self.m_button5, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.m_button6 = wx.Button(self, wx.ID_ANY, u"clear MyFavorite", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer1.Add(self.m_button6, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        bSizer1.Add(gSizer1, 1, wx.EXPAND, 5)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.Bind(wx.EVT_CLOSE, self.saveAll)
+        self.m_button1.Bind(wx.EVT_BUTTON, self.generate)
+        self.m_button2.Bind(wx.EVT_BUTTON, self.copyto)
+        self.m_button3.Bind(wx.EVT_BUTTON, self.add)
+        self.m_button4.Bind(wx.EVT_BUTTON, self.favoritegenerate)
+        self.m_button5.Bind(wx.EVT_BUTTON, self.remove)
+        self.m_button6.Bind(wx.EVT_BUTTON, self.clear)
+
+    def __del__(self):
+        pass
+
+    # Virtual event handlers, overide them in your derived class
+    def saveAll(self, event):
+        event.Skip()
+
+    def generate(self, event):
+        event.Skip()
+
+    def copyto(self, event):
+        event.Skip()
+
+    def add(self, event):
+        event.Skip()
+
+    def favoritegenerate(self, event):
+        event.Skip()
+
+    def remove(self, event):
+        event.Skip()
+
+    def clear(self, event):
+        event.Skip()
